f673c54d80de2e30c87f0d22e5a42ef7b1098229
Free-Swallow <nickphoenix21@icloud.com>
2022-08-24 01:14:33 +0700

    Добавил функцию случайного числа, поправил функцию проверки длинны строки

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-const checkLength = (checkString, maxLength) => checkString.length <= maxLength;
 
-console.log(checkLength('251', 4));
+// Функция, возвращающая случайное целое число из переданного диапазона включительно.
+
+const getRandomPositiveInteger = (valueA = 0, valueB = 25) => {
+  const lower = Math.ceil(Math.min(Math.abs(valueA), Math.abs(valueB)));
+  const upper = Math.floor(Math.max(Math.abs(valueA), Math.abs(valueB)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// Функция для проверки максимальной длины строки
+
+const isCheckLength = (checkString, maxLength = 200) => checkString.length <= maxLength;
+
+isCheckLength('251', getRandomPositiveInteger());
